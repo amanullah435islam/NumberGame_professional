610692edf91d47bc9a122f0c17f6801d25733164
Md. Amanullah Islam <amanullah435islam@gmail.com>
2026-07-08 12:01:07 +0600

    Professional Step 7

--- src/main/java/org/testProjectImranSir/Main.java
+++ src/main/java/org/testProjectImranSir/Main.java
@@ -4,16 +4,47 @@ import com.decodelabs.numbergame.Difficulty;
 import com.decodelabs.numbergame.Game;
 import com.decodelabs.numbergame.InputHelper;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) {
 
-        Game game = new Game();
-        game.startGame();
+        Scanner scanner = new Scanner(System.in);
 
+        Game game = new Game();
+
+        boolean playAgain = true;
+
+        while (playAgain) {
+
+            game.startGame();
+
+            System.out.println();
+            System.out.print("Do you want to play again? (Y/N): ");
+
+            String choice = scanner.next();
+
+            if (choice.equalsIgnoreCase("Y")) {
+
+                playAgain = true;
+
+            } else {
+
+                playAgain = false;
+
+            }
+
+        }
+
+        System.out.println();
+        System.out.println("Thank you for playing!");
+        scanner.close();
 
     }
+
 
 
 }
